85648c336de0cfa779f6874bda2d65e7e1e3700e
BaconXT <yuripacritter@gmail.com>
2023-06-01 20:41:06 -0300

    usando github copilot de um jeito inteligente

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>O Vietnã é muito bom, por que *SIM*</Text>
       <StatusBar style="auto" />
     </View>
   );
